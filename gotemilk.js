2d63bf82df61ad191c46a19c1326a6c0cc1c935a
gotemilk <44422917+gotemilk@users.noreply.github.com>
2020-08-04 21:38:25 -0600

    Update gotemilk.js

--- gotemilk.js
+++ gotemilk.js
@@ -1,10 +1,10 @@
 var countDownDate = new
-Date("Aug 3, 2020 23:59:59").getTime();var x = setInterval(function() {var now = new
+Date("Aug 21, 2020 23:59:59").getTime();var x = setInterval(function() {var now = new
 Date().getTime();var distance = countDownDate - now;var days =
 Math.floor(distance / (1000 * 60 * 60 * 24));var hours = Math.floor((distance % (1000
 * 60 * 60 * 24)) / (1000 * 60 * 60));var minutes = Math.floor((distance % (1000 * 60 *
 60)) / (1000 * 60));var seconds = Math.floor((distance % (1000 * 60)) /
 1000);document.getElementById("timer").innerHTML = days + "d " + hours + "h "+
 minutes + "m " + seconds + "s ";if (distance < 0)
-{clearInterval(x);document.getElementById("timer").innerHTML = "Webstore Closed. Additional Purchases Will Be Reserved for Artwalk September 4th.";} }, 1000); 
+{clearInterval(x);document.getElementById("timer").innerHTML = "Thank you so much";} }, 1000); 
 
